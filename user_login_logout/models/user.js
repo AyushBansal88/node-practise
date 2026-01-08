b309a52e7964bfe639a2320e1516c095091333da
Ayush Bansal <ayushbansal0588@gmail.com>
2026-01-08 15:37:51 +0530

    user login logout with jwt and otp complete

---
+++ user_login_logout/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose'); 
+
+mongoose.connect(`mongodb://127.0.0.1:27017/authtestapp`);
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true 
+    },
+    password: String,
+    age: Number,
+    otp: Number
+})
+
+module.exports = mongoose.model('user', userSchema);
